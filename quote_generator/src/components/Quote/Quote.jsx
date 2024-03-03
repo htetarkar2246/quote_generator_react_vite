095824566477fc1bd8c03295c17b4375054197b8
Htet ArKar <115476708+htetarkar2246@users.noreply.github.com>
2024-03-03 17:47:17 +0630

    quote

--- quote_generator/src/components/Quote/Quote.jsx
+++ quote_generator/src/components/Quote/Quote.jsx
@@ -27,34 +27,32 @@ function Quote() {
         navigator.clipboard.writeText(`${quote} - ${author}`);
     };
 
-    const shareToFacebook = () => {
-        const url = `https://www.facebook.com/sharer/sharer.php?quote=${quote} - ${author}`;
-        window.open(url, '_blank');
-    };
-
     return (
-        <div className='container rounded shadow p-3 w-50'>
-            <h1>Quote Generator</h1>
-            {quote ? (
-                <>
-                    <p className='quote-text'>{`\u275D${quote}\u275E`}</p>
-                    <p className="author">{`By - ${author}`}</p>
-                    <div className='d-flex justify-content-center align-items-center mt-4'>
-                        <button onClick={speakQuote} className='btn action-btn'>
-                            <i className="fa-solid fa-volume-high"></i>
-                        </button>
-                        <button onClick={copyQuote} className='btn action-btn'>
-                            <i className="fa-solid fa-copy"></i>
-                        </button>
-                        <button onClick={shareToFacebook} className='btn action-btn'>
-                            <i className="fa-brands fa-facebook"></i>
-                        </button>
+        <div className='container-fluid p-3'>
+            <div className='row justify-content-center'>
+                <div className='col-md-8'>
+                    <div className='container rounded shadow p-3'>
+                        <h1>Quote Generator</h1>
+                        {quote ? (
+                            <>
+                                <p className='quote-text'>{`\u275D${quote}\u275E`}</p>
+                                <p className="author">{`By - ${author}`}</p>
+                                <div className='d-flex justify-content-center align-items-center mt-4'>
+                                    <button onClick={speakQuote} className='btn action-btn'>
+                                        <i className="fa-solid fa-volume-high"></i>
+                                    </button>
+                                    <button onClick={copyQuote} className='btn action-btn'>
+                                        <i className="fa-solid fa-copy"></i>
+                                    </button>
+                                </div>
+                            </>
+                        ) : (
+                            <p>Please click <b>New Quote</b> to generate new quote. </p>
+                        )}
+                        <button onClick={generateQuote} className='btn btn-primary mt-3'>New Quote</button>
                     </div>
-                </>
-            ) : (
-                <p>Please click <b>New Quote</b> to generate new quote. </p>
-            )}
-            <button onClick={generateQuote} className='btn' id='new_quote_btn'>New Quote</button>
+                </div>
+            </div>
         </div>
     );
 }
